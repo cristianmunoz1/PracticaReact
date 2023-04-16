cfad356546b5c4bb15ebd57de6925a91e9afd140
Cristian Muñoz <cristian.munoz3@udea.edu.co>
2023-04-15 23:24:49 -0500

    Realizamos el formulario y realizamos un ejemplo de como agregar productos, solo con el título, a partir del formulario html

--- src/components/ProductsComponent.jsx
+++ src/components/ProductsComponent.jsx
@@ -4,6 +4,14 @@ export const ProductsComponent = (props) => {
 
     const [productList, setProductList] = useState([]);
 
+    const [ productForm, setProductForm] = useState({
+        title: "",
+        price: 0,
+        description: "",
+        image: "",
+        category: ""
+    });
+
     const handleGetAllProducts = async () => {
         const products = await fetch('https://fakestoreapi.com/products')
         .then( res=>res.json() )
@@ -14,9 +22,60 @@ export const ProductsComponent = (props) => {
         setProductList(products);
     }
 
+    const submitNewProduct = (event) => {
+        event.preventDefault();
+        console.log({productForm});
+
+        fetch('https://fakestoreapi.com/products',{
+            method:"POST",
+            body:JSON.stringify(
+                {
+                    title: productForm.title,
+                    price: 13.5,
+                    description: 'lorem ipsum set',
+                    image: 'https://i.pravatar.cc',
+                    category: 'electronic'
+                }
+            )
+        })
+            .then(res=>res.json())
+            .then(json=>console.log(json))
+    }
+
+    const handleInputChange = (event) => {
+        console.log(event.target);
+        const target = event.target;
+
+        const value = target.type === "checkbox" ? target.checked : target.value;
+        const name = target.name;
+
+        setProductForm({
+            ...productForm,
+            [name]: value 
+        });
+    }
+
+    useEffect( () => {
+        console.log({productForm})
+    }, [productForm])
+
     return (
         <div>
             <h1>Productos</h1>
+
+            <form className="formulario" onSubmit={submitNewProduct}>
+                <label htmlFor="title">Título: </label>
+                <input type="text" id="title" name="title" 
+                placeholder="Título" required 
+                value={productForm.title}
+                onChange={handleInputChange}
+             />
+
+             <button type="submit">Registrar producto</button>
+            </form>
+
+            <hr />
+
 
             <button onClick={handleGetAllProducts}>
                 Traer todos los productos
