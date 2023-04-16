46cba1b05299820b13cd787158cceafa20835e1f
Cristian Muñoz <cristian.munoz3@udea.edu.co>
2023-04-15 21:15:43 -0500

    Versión base clonada del repositorio necesario para la clase

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { WelcomeComponent } from './components/WelcomeComponent'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -13,6 +14,8 @@ function App() {
         <button onClick={() => setCount((count) => count + 1)}>
           Soy un contador {count}
         </button>
+        <hr />
+        <WelcomeComponent usuarioActivo={true} name="Cristian"/>
       </div>
     </div>
   )
---
+++ src/components/WelcomeComponent.jsx
@@ -0,0 +1,9 @@
+export function WelcomeComponent(props){
+  return (
+    <div>
+      <h1>Welcome component { props.name }</h1>
+
+      
+    </div>
+  )
+}
