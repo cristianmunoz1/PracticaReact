7a015cbe4f7ddf538f31c0b09e09ed8fc6a885ee
Cristian Muñoz <cristian.munoz3@udea.edu.co>
2023-04-15 21:57:42 -0500

    Explicación de los hooks useState y useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,34 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import { WelcomeComponent } from './components/WelcomeComponent'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const [userName, setUsername] = useState("");
+  const [ isUserLogged, setIsUserLogged] = useState(false);
+
+  useEffect(()=> {
+    console.log("Cambiando is user log");
+    setIsUserLogged(true);
+  }, [])
+
+  useEffect(() => {
+    if(count == 1500) {
+      console.log("El count llegó a 1500");
+    }
+  }, [count])
 
   return (
     <div className="App">
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button onClick={() => setCount((count) => count + 500)}>
           Soy un contador {count}
         </button>
         <hr />
-        <WelcomeComponent usuarioActivo={true} name="Cristian"/>
+        {(count > 1000) && <WelcomeComponent usuarioActivo = {true}/>}
       </div>
     </div>
   )
